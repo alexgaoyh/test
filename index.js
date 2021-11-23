15de7611804c50d95980c78d4b714ab96e18465e
alexgaoyh <alexgaoyh@sina.com>
2021-11-23 14:40:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var rightBtn = false
 // 创建地图实例
 var map = new BMapGL.Map("container", {
 		minZoom : 3,
-		maxZoom : 9
+		maxZoom : 15
 	});
 // 初始化地图,设置中心点坐标和地图级别
 map.centerAndZoom(new BMapGL.Point(116.404, 39.915), 7);
